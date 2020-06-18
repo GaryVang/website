d6c2ae91abfc6fc9438d4a3c3879857a4ecafcfa
GaryVang <leng_vang89@hotmail.com>
2020-06-18 01:40:45 -0700

    Card flip layout added.

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -1,12 +1,33 @@
 import React, { useState } from 'react';
+import './Home.css';
+import TempImg from '../Game/Witcher3/witcher_3.jpg';
 
 const Home = ({}) => {
 
     return (
-        <section>
-            <h2>Welcome to G's Website</h2>
-            <h2>I am G</h2>
-            <h5>And I'm a Developer, Gamer, and an occasional Cyclist</h5>
+        <section className='home-container'>
+            <header>
+                <h2>Welcome!</h2>
+            </header>
+            <div className='home-intro-container'>
+                <div className='home-intro-inner'>
+                    <h2>I am G</h2>
+                    <h4>And I'm a Developer, Gamer, and an occasional Cyclist</h4>
+                </div>
+                
+            </div>
+            {/* <div class="flip-card">
+                <div class="flip-card-inner">
+                    <div class="flip-card-front">
+                        <img className='avatar-img' src={TempImg} alt="Avatar" />
+                    </div>
+                    <div class="flip-card-back">
+                        <h1>John Doe</h1> 
+                        <p>Architect & Engineer</p> 
+                        <p>We love that guy</p>
+                    </div>
+                </div>
+            </div> */}
         </section>
     );
 };
