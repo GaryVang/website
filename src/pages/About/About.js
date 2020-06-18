78947d4aca5ee8309d90e087bc5d32481e996c9e
MysticV <leng_vang89@hotmail.com>
2020-06-17 18:18:56 -0700

    Added background to About Me page and some layout tests.

--- src/pages/About/About.js
+++ src/pages/About/About.js
@@ -1,11 +1,19 @@
 import React from 'react';
 import './About.css';
+import BGTrees from './bg-trees.jpg';
+
 const About = ({}) => {
     return (
         <section className='about-container'>
             <header>
-                <h2>About Me</h2>
+                <h2 className='about-header'>About Me</h2>
             </header>
+            <section className='about-sect'>
+                <article className='about-sect-art'>
+                    <img className='about-sect-art-img' src={BGTrees} alt='Trees' />
+                    <p>Hello There</p>
+                </article>
+            </section>
         </section>
     );
 };
