a27cd03f902dade6454d51416dca4d9eb7627672
MysticV <leng_vang89@hotmail.com>
2020-06-17 18:33:37 -0700

    New Home page header.

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -5,8 +5,12 @@ const Home = ({}) => {
     return (
         <section>
             <h2>Welcome to G's Website</h2>
+            <h2>I am G</h2>
+            <h5>And I'm a Developer, Gamer, and an occasional Cyclist</h5>
         </section>
     );
 };
 
 export default Home;
+
+// https://www.w3schools.com/howto/tryit.asp?filename=tryhow_css_blurred_bg
